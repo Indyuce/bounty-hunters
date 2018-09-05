0dfde6573fae74760feecdc00801f1384390245c
Indyuce <Cym.peyrat@yahoo.fr>
2018-09-05 18:01:54 +0200

    removed classes

--- net/Indyuce/bountyhunters/nms/json/Json.java
+++
@@ -1,7 +0,0 @@
-package net.Indyuce.bountyhunters.nms.json;
-
-import org.bukkit.entity.Player;
-
-public interface Json {
-	public void message(Player player, String message);
-}
--- net/Indyuce/bountyhunters/nms/nbttag/NBTTags.java
+++
@@ -1,9 +0,0 @@
-package net.Indyuce.bountyhunters.nms.nbttag;
-
-import org.bukkit.inventory.ItemStack;
-
-public interface NBTTags {
-	public ItemStack add(ItemStack i, ItemTag... tags);
-
-	public String getStringTag(ItemStack i, String path);
-}
