2e92ac783a8680fd5c0bff338aa050d7ba601936
Indyuce <cym.peyrat@yahoo.fr>
2019-10-13 12:38:48 +0200

    added manager class

---
+++ src/net/Indyuce/bountyhunters/comp/database/bounty/MySQLBountyManager.java
@@ -0,0 +1,18 @@
+package net.Indyuce.bountyhunters.comp.database.bounty;
+
+import net.Indyuce.bountyhunters.manager.BountyManager;
+
+public class MySQLBountyManager extends BountyManager {
+
+	@Override
+	public void loadBounties() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveBounties() {
+		// TODO Auto-generated method stub
+		
+	}
+}
