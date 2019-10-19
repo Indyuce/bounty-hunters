85de1a0b85487696df6d5b5b63671c12c901ab94
Indyuce <cym.peyrat@yahoo.fr>
2019-10-19 21:35:06 +0200

    fixed import issue

--- src/net/Indyuce/bountyhunters/api/BountyEffect.java
+++ src/net/Indyuce/bountyhunters/api/BountyEffect.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.UUID;
 import java.util.logging.Level;
 
-import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
