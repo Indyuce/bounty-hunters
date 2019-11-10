4a323e83f00f169d9e95ae10232d095b5c785829
Indyuce <cym.peyrat@yahoo.fr>
2019-11-10 18:58:27 +0100

    Added teammate and spawn point claim restrictions

---
+++ src/net/Indyuce/bountyhunters/api/restriction/BedSpawnPoint.java
@@ -0,0 +1,22 @@
+package net.Indyuce.bountyhunters.api.restriction;
+
+import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+import net.Indyuce.bountyhunters.api.Bounty;
+
+public class BedSpawnPoint implements ClaimRestriction {
+	private final int radiusSquared;
+
+	public BedSpawnPoint(ConfigurationSection config) {
+		radiusSquared = config.getInt("radius") * config.getInt("radius");
+	}
+
+	@Override
+	public boolean canClaimBounty(Player claimer, Bounty bounty) {
+		Location loc = claimer.getBedSpawnLocation();
+		Location loc1 = bounty.getTarget().getBedSpawnLocation();
+		return loc == null || loc1 == null || loc.distanceSquared(loc1) < radiusSquared;
+	}
+}
