c477f717992caf7a24ead11fb159b411cd2247ab
Indyuce <cym.peyrat@yahoo.fr>
2022-01-15 13:39:22 +0100

    cleanup

--- bounty-hunters-plugin/src/main/java/net/Indyuce/bountyhunters/api/OfflineProfile.java
+++ bounty-hunters-plugin/src/main/java/net/Indyuce/bountyhunters/api/OfflineProfile.java
@@ -1,90 +1,96 @@
 package net.Indyuce.bountyhunters.api;
 
+import org.antlr.v4.runtime.misc.NotNull;
+import org.apache.commons.lang.Validate;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
+import java.util.Objects;
 import java.util.UUID;
 
 /**
  * Not used yet but here if BH needs to stop using OfflinePlayers and migrate to
  * Name/UUID duos to store player data
- * 
- * @author cympe
  *
+ * @author cympe
  */
 public class OfflineProfile {
+    private final UUID uuid;
+    private final String name;
+    private final OfflinePlayer offline;
 
-	private final UUID uuid;
+    /**
+     * Solves the problem of trying to retrieve OfflinePlayers when loading
+     * bounties. If the offline player is found then we can access its name,
+     * otherwise we can't.
+     * <p>
+     * Displaying player skulls require the OfflinePlayer instance.
+     * <p>
+     * Added in 2.3.15
+     *
+     * @param player Offline player
+     */
+    public OfflineProfile(OfflinePlayer player) {
+        this.uuid = player.getUniqueId();
+        this.name = player.getName();
+        this.offline = player;
+    }
 
-	private final String name;
-	private final OfflinePlayer offline;
+    /**
+     * @param obj Either a string of a configuration section to support both new
+     *            and old bounty data formats (2.3.15 or earlier)
+     */
+    public OfflineProfile(Object obj) {
 
-	/**
-	 * Solves the problem of trying to retrieve OfflinePlayers when loading
-	 * bounties. If the offline player is found then we can access its name,
-	 * otherwise we can't.
-	 * 
-	 * Displaying player skulls require the OfflinePlayer instance.
-	 * 
-	 * Added in 2.3.15
-	 * 
-	 * @param uuid The player UUID
-	 */
-	public OfflineProfile(OfflinePlayer player) {
-		this.uuid = player.getUniqueId();
-		this.name = player.getName();
-		this.offline = player;
-	}
+        if (obj instanceof String) {
+            uuid = UUID.fromString((String) obj);
+            offline = Bukkit.getOfflinePlayer(uuid);
+            name = isFound() ? getOfflinePlayer().getName() : "Name?";
+            return;
+        }
 
-	/**
-	 * @param obj Either a string of a configuration section to support both new
-	 *            and old bounty data formats (2.3.15 or earlier)
-	 */
-	public OfflineProfile(Object obj) {
+        if (obj instanceof ConfigurationSection) {
+            ConfigurationSection config = (ConfigurationSection) obj;
+            uuid = UUID.fromString(config.getString("uuid"));
+            name = config.getString("name");
+            offline = Bukkit.getOfflinePlayer(uuid);
+            return;
+        }
 
-		if (obj instanceof String) {
-			uuid = UUID.fromString((String) obj);
-			offline = Bukkit.getOfflinePlayer(uuid);
-			name = isFound() ? getOfflinePlayer().getName() : "Name?";
-			return;
-		}
+        throw new IllegalArgumentException("Provide either a string or a config section");
+    }
 
-		if (obj instanceof ConfigurationSection) {
-			ConfigurationSection config = (ConfigurationSection) obj;
-			uuid = UUID.fromString(config.getString("uuid"));
-			name = config.getString("name");
-			offline = Bukkit.getOfflinePlayer(uuid);
-			return;
-		}
+    public UUID getUniqueId() {
+        return uuid;
+    }
 
-		throw new IllegalArgumentException("Provide either a string or a config section");
-	}
+    public String getName() {
+        return name;
+    }
 
-	public UUID getUniqueId() {
-		return uuid;
-	}
+    @NotNull
+    public OfflinePlayer getOfflinePlayer() {
+        return Objects.requireNonNull(offline, "Offline player not found");
+    }
 
-	public String getName() {
-		return name;
-	}
+    public ItemStack displaySkull() {
+        Validate.notNull(offline, "Skull owner not found");
 
-	public OfflinePlayer getOfflinePlayer() {
-		return offline;
-	}
+        ItemStack skull = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
+        skullMeta.setOwningPlayer(offline);
+        skull.setItemMeta(skullMeta);
+        return skull;
+    }
 
-	public void displaySkull(ItemStack item, ItemMeta meta) {
-		if (offline == null)
-			return;
-
-	}
-
-	/**
-	 * @return Checks if the OfflinePlayer was found
-	 */
-	public boolean isFound() {
-		return offline != null;
-	}
+    /**
+     * @return Checks if the OfflinePlayer was found
+     */
+    public boolean isFound() {
+        return offline != null;
+    }
 }
