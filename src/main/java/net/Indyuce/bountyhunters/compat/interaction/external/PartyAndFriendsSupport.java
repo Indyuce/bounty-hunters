71e38d2ad1f10217ec8efea867d2b546d8460b58
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-09-20 01:48:13 +0700

    Fix maven Fix hook with friends

--- src/main/java/net/Indyuce/bountyhunters/compat/interaction/external/PartyAndFriendsSupport.java
+++ src/main/java/net/Indyuce/bountyhunters/compat/interaction/external/PartyAndFriendsSupport.java
@@ -1,15 +1,15 @@
 package net.Indyuce.bountyhunters.compat.interaction.external;
 
+import de.simonsator.partyandfriends.spigot.api.pafplayers.PAFPlayerManager;
 import net.Indyuce.bountyhunters.compat.interaction.InteractionRestriction;
-import de.simonsator.partyandfriends.api.pafplayers.PAFPlayerManager;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 public class PartyAndFriendsSupport implements InteractionRestriction {
-	private final PAFPlayerManager manager = PAFPlayerManager.getInstance();
+    private final PAFPlayerManager manager = PAFPlayerManager.getInstance();
 
-	@Override
-	public boolean canInteractWith(InteractionType interaction, Player claimer, OfflinePlayer target) {
-		return !manager.getPlayer(claimer.getUniqueId()).isAFriendOf(manager.getPlayer(target.getUniqueId()));
-	}
+    @Override
+    public boolean canInteractWith(InteractionType interaction, Player claimer, OfflinePlayer target) {
+        return !manager.getPlayer(claimer.getUniqueId()).isAFriendOf(manager.getPlayer(target.getUniqueId()));
+    }
 }
